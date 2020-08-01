5f253831a35c19f35e3f3ab0a9e63a3f724cb2f9
Chingiz Mamedov <38461842+chingizmamedov@users.noreply.github.com>
2020-08-01 19:45:18 +0400

    add build

---
+++ build/precache-manifest.f6da0b995fd3c2596503fae43e478994.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9743754e06c69262bf6ce025b5fe303",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17ae5d2893a46995fa79",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ef7b3f40652f49b417ea",
+    "url": "/static/css/main.b6d9443b.chunk.css"
+  },
+  {
+    "revision": "17ae5d2893a46995fa79",
+    "url": "/static/js/2.00306806.chunk.js"
+  },
+  {
+    "revision": "4a71bd6b92677535073f225a3e308c8a",
+    "url": "/static/js/2.00306806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7b3f40652f49b417ea",
+    "url": "/static/js/main.734f0d56.chunk.js"
+  },
+  {
+    "revision": "83a2e22fc6c80be2ed1c",
+    "url": "/static/js/runtime-main.ec569187.js"
+  },
+  {
+    "revision": "81b6e769e24a81792c4e67496ea77006",
+    "url": "/static/media/kostyum.81b6e769.jpg"
+  },
+  {
+    "revision": "ae5dc19f083e8667a24b6590726d1ad9",
+    "url": "/static/media/login-shop.ae5dc19f.png"
+  },
+  {
+    "revision": "3eef3c7d8ccdde4bab54b8e6da010688",
+    "url": "/static/media/rubashka.3eef3c7d.jpg"
+  },
+  {
+    "revision": "ab3b3146c3e0d77ab3d40bc737074cfd",
+    "url": "/static/media/shoes.ab3b3146.webp"
+  },
+  {
+    "revision": "d36a82718a8aa3b81eec1df9e506d6c5",
+    "url": "/static/media/ust-geyimi.d36a8271.jpg"
+  }
+]);
